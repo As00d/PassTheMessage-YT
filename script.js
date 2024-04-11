becb74bc19c58f4b958bafa99f75b8ff4031d29b
Ankita Sood <ankisood@amazon.com>
2024-04-11 22:46:45 +0530

    PassTheMessage

---
+++ script.js
@@ -0,0 +1,8 @@
+const getMessage = function () {
+  let value = document.getElementById("message").value;
+  console.log(value);
+  document.getElementById("textMessage").textContent = value;
+  var element = document.getElementById("textMessage");
+  element.classList.add("textDisplayed");
+  element.classList.remove("textHidden");
+};
